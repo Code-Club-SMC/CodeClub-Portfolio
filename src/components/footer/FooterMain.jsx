c34e5c66bdd57e8e74136f1d6da691beede987fa
Abirullah <abirafridi87@gmail.com>
2026-08-21 15:25:41 +0500

    Update portfolio

--- src/components/footer/FooterMain.jsx
+++ src/components/footer/FooterMain.jsx
@@ -167,7 +167,7 @@ const FooterMain = ({ className }) => {
           >
             <FaMagnifyingGlassLocation className="text-blue-600 text-xl mt-1 group-hover:text-blue-700 transition-colors" />
             <p className="group-hover:text-blue-700 transition-colors">
-             Offic No.50 2nd floor Liberty Mall Opp: Airport runway, University Rd, Tehkal,
+             Office No.50 2nd floor Liberty Mall Opp: Airport runway, University Rd, Tehkal,
               Peshawar, 25000, Pakistan
             </p>
           </a>
